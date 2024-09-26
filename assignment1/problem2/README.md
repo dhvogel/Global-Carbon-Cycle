a2ebe818a239c9539d6ea372c605309135b33a9e
Dan Vogel <dhvogel2468@gmail.com>
2024-09-26 18:17:22 -0400

    problem 2

---
+++ assignment1/problem2/README.md
@@ -0,0 +1,10 @@
+# Problem 2
+
+```shell
+go get -u
+go run main.go
+```
+
+# Output
+
+![output](./michaelis_menten.png "Output")
---
+++ assignment1/problem2/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+)
+
+// MichaelisMenten calculates the reaction rate V for a given substrate concentration [S].
+func MichaelisMenten(S, Vmax, Km float64) float64 {
+	return (Vmax * S) / (Km + S)
+}
+
+func main() {
+	// Parameters for the Michaelis-Menten equation
+	Vmax := 1.0 // Assume VMax is 1
+	Km := 0.5 // Michaelis constant is half VMax
+
+	// Create a plot
+	p := plot.New()
+
+	p.Title.Text = "Michaelis-Menten"
+	p.X.Label.Text = "Substrate Concentration [S]"
+	p.Y.Label.Text = "Reaction Rate (V)"
+
+	// Create a line plot for Michaelis-Menten
+	points := make(plotter.XYs, 100)
+	for i := 0; i < 100; i++ {
+		S := float64(i) * 0.05 // Varying substrate concentration
+		points[i].X = S
+		points[i].Y = MichaelisMenten(S, Vmax, Km)
+	}
+
+	// Create a line to represent the data
+	line, err := plotter.NewLine(points)
+	if err != nil {
+		panic(err)
+	}
+
+	p.Add(line)
+
+	// Save the plot to a PNG file
+	if err := p.Save(6*vg.Inch, 6*vg.Inch, "michaelis_menten.png"); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Michaelis-Menten plot saved as 'michaelis_menten.png'")
+}
